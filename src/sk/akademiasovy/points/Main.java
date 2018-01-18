212882d699b18b4343772e8fb163b054892248d0
Ondrej Tomco <ondrej.tomco@akademiasovy.sk>
2018-01-18 12:12:40 +0100

    Initial commit

---
+++ src/sk/akademiasovy/points/Main.java
@@ -0,0 +1,21 @@
+package sk.akademiasovy.points;
+
+public class Main {
+    public static void main(String[] args) {
+        Point p1 = new Point(8,12);
+        System.out.println("Point p1 is in " + p1.getQuadrant() + " quadrant");
+
+        Point p2 = new Point();
+        p2.generateRandomCor();
+
+        System.out.println("Point p2 is in " + p2.getQuadrant() + " quadrant");
+        System.out.println(p2.getDistance());
+        p2.printInfo();
+        System.out.println("Distance between points: " + p2.getDistanceFromOtherPoint(p1));
+        Point3d point1 = new Point3d();
+        point1.generateRandomCor();
+        point1.printInfo();
+
+
+    }
+}
